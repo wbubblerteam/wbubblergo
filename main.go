9cd7fb6d38b72657d3a80c58a5287bcb858e7d2b
SmilerRyan <ryanthornton03@gmail.com>
2021-10-04 16:59:47 +0100

    Update main.go
    Add Multiple Lines (consistent across all packages).

--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello, i am Wbubbler, your friend ! bub is cool. tell friends to install me i am funny. wbubbler 1.0.3")
+	fmt.Println(`hello, i am Wbubbler, your friend !
+bub is cool.
+tell friends to install me i am funny.`)
 }
